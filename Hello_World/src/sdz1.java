68645648ef658f444dc4e48484dd03fffe50b8cc
Su YANG <ys19901010@hotmail.fr>
2016-06-23 00:07:33 +0200

    first commit

---
+++ Hello_World/src/sdz1.java
@@ -0,0 +1,11 @@
+
+public class sdz1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World ! \n");
+		System.out.println("My name is");
+		System.out.println("\nCysboy");
+	}
+
+}
